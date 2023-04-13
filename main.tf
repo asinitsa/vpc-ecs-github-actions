dd2a13ee56061c3a65a3d7c36d34036ed996b5a7
Artsiom Sinita <arsinitsa@outlook.com>
2023-04-13 19:08:47 +0200

    create alb

--- main.tf
+++ main.tf
@@ -40,3 +40,45 @@ module "vpc" {
     Environment = "dev"
   }
 }
+
+resource "aws_security_group" "allow-http" {
+  name_prefix = "dev-allow-80-"
+  description = "Allow HTTP inbound traffic"
+  vpc_id      = module.vpc.vpc_id
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  tags = {
+    Terraform   = "true"
+    Environment = "dev"
+  }
+}
+
+resource "aws_lb" "dev" {
+  name               = "dev-alb"
+  internal           = false
+  load_balancer_type = "application"
+  security_groups = [
+    aws_security_group.allow-http.id
+  ]
+  subnets = module.vpc.public_subnets
+
+  tags = {
+    Name        = "dev-alb",
+    Environment = "dev"
+  }
+
+}
