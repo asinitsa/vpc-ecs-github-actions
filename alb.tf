600f9737c0c2b286f5776b55b0919ba8f2dc2238
Artsiom Sinita <arsinitsa@outlook.com>
2023-04-13 19:57:31 +0200

    create ecs

--- alb.tf
+++ alb.tf
@@ -36,7 +36,7 @@ resource "aws_lb_target_group" "nginx" {
   port        = 80
   protocol    = "HTTP"
   vpc_id      = module.vpc.vpc_id
-  target_type = "instance"
+  target_type = "ip"
   health_check {
     path    = "/"
     port    = "80"
